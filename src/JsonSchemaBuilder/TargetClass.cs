de025ef2f1a5cc879f681c45be23112753289893
carlos.angulo.mascarell <carlos.angulo.mascarell@outlook.com>
2021-10-14 22:55:26 +0200

    fix: INIT

---
+++ src/JsonSchemaBuilder/TargetClass.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JsonSchemaBuilder
+{
+    public class TargetClassToCreateSchema
+    {
+        public string[] ConventionalCommitTypes { get; set; }
+        public string CommitMsgRegexValidator { get; set; }
+        public bool OutputCommitList { get; set; }
+        public string OutputCommitListPath { get; set; } = "commits.txt";
+    }
+}
